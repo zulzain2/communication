68123cf6fcbc94755c196996fd6e92680616aad2
ZulZayn <zulwaqarzain96@gmail.com>
2021-06-03 13:03:58 +0800

    room ID + hex 32

--- app/Models/MeetingLog.php
+++ app/Models/MeetingLog.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class MeetingLog extends Model
 {
-    use HasFactory;
+    
+    use Notifiable;
+    protected $table = 'meeting_logs';
+    public $incrementing = FALSE;
 
     public function scheduler()
     {
